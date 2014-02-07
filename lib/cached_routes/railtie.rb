72b4065bbfdb21e1e83d5a15348aea0d6dadd034
David McCullars <dmccullars@bloomfire.com>
2014-02-06 18:24:21 -0600

    Use singleton_class for railtie for simplicity

--- lib/cached_routes/railtie.rb
+++ lib/cached_routes/railtie.rb
@@ -2,11 +2,7 @@ module CachedRoutes
   class Railtie < Rails::Railtie
 
     initializer "add_caching_of_routes" do |app|
-      app.routes.instance_eval do
-        class << self
-          include RouteSet
-        end
-      end
+      app.routes.singleton_class.send :include, RouteSet
     end
 
   end
